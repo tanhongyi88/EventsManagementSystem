f5961ff0e5c94c970dce6235c3cea52fcdab0a05
tanhongyi88 <hongyi.tan88@gmail.com>
2024-04-18 22:13:29 +0800

    added category and event recycler card views

---
+++ app/src/main/java/com/fit2081/assignment1/RecyclerAdapterCategory.java
@@ -0,0 +1,57 @@
+package com.fit2081.assignment1;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapterCategory extends RecyclerView.Adapter<RecyclerAdapterCategory.CategoryItemViewHolder> {
+
+    ArrayList<Category> categoryList = new ArrayList<>();
+
+    public void setCategoryList(ArrayList<Category> categoryList) {
+        this.categoryList = categoryList;
+    }
+
+    @NonNull
+    @Override
+    public CategoryItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_category_item, parent, false); //CardView inflated as RecyclerView list item
+        CategoryItemViewHolder viewHolder = new CategoryItemViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryItemViewHolder holder, int position) {
+        holder.idView.setText(categoryList.get(position).getId());
+        holder.nameView.setText(categoryList.get(position).getName());
+        holder.eventCountView.setText(categoryList.get(position).getEventCount());
+        holder.isActiveView.setText(categoryList.get(position).isActive() ? "Active" : "Inactive");
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class CategoryItemViewHolder extends RecyclerView.ViewHolder {
+        public TextView idView;
+        public TextView nameView;
+        public TextView eventCountView;
+        public TextView isActiveView;
+
+        public CategoryItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            idView = itemView.findViewById(R.id.category_item_id_view);
+            nameView = itemView.findViewById(R.id.category_item_name_view);
+            eventCountView = itemView.findViewById(R.id.category_item_event_count_view);
+            isActiveView = itemView.findViewById(R.id.category_item_isactive_view);
+        }
+    }
+}
---
+++ app/src/main/java/com/fit2081/assignment1/RecyclerAdapterEvent.java
@@ -0,0 +1,58 @@
+package com.fit2081.assignment1;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapterEvent extends RecyclerView.Adapter<RecyclerAdapterEvent.EventItemViewHolder> {
+
+    ArrayList<Event> eventList = new ArrayList<>();
+
+    public void setEventList(ArrayList<Event> eventList) {
+        this.eventList = eventList;
+    }
+
+    @NonNull
+    @Override
+    public EventItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_event_item, parent, false); //CardView inflated as RecyclerView list item
+        EventItemViewHolder viewHolder = new EventItemViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventItemViewHolder holder, int position) {
+        holder.idView.setText(eventList.get(position).getId());
+        holder.nameView.setText(eventList.get(position).getName());
+        holder.categoryIdView.setText(eventList.get(position).getCategoryId());
+        holder.ticketsView.setText(String.valueOf(eventList.get(position).getTicketsAvailable()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventList.size();
+    }
+
+    public class EventItemViewHolder extends RecyclerView.ViewHolder {
+        public TextView idView;
+        public TextView nameView;
+        public TextView categoryIdView;
+        public TextView ticketsView;
+        public TextView isActive;
+
+        public EventItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            idView = itemView.findViewById(R.id.event_item_id_view);
+            nameView = itemView.findViewById(R.id.event_item_name_view);
+            categoryIdView = itemView.findViewById(R.id.event_item_category_id_view);
+            ticketsView = itemView.findViewById(R.id.event_item_tickets_view);
+            isActive = itemView.findViewById(R.id.event_item_is_active_view);
+        }
+    }
+}
